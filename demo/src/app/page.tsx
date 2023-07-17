679c5fb383c8e76e4d4770150148cc6d9888fd90
Natterum <fast7creation@gmail.com>
2023-07-17 11:43:58 +0200

    Update demo file

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-table-scroll/dist/assets/index.min.css';
 import { ReactTableScroll } from 'react-table-scroll';
 import { Container } from 'react-bootstrap';
 
@@ -13,36 +12,28 @@ export default function Home() {
                     <thead>
                         <tr>
                             <td>ID</td>
-                            <td width="25%">Name</td>
-                            <td width="25%"> Surname</td>
-                            <td>ID</td>
-                            <td width="25%">Name</td>
-                            <td width="25%">Surname</td>
-                            <td>ID</td>
-                            <td width="25%">Name</td>
-                            <td width="25%"> Surname</td>
-                            <td>ID</td>
-                            <td width="25%">Name</td>
-                            <td width="25%">Surname</td>
+                            <td width="5%">Name</td>
+                            <td width="5%">Last Name</td>
+                            <td width="5%">Surname</td>
+                            <td width="25%">Passport ID</td>
+                            <td width="25%">Company</td>
+                            <td width="25%">Address</td>
+                            <td width="25%">Country</td>
                         </tr>
                     </thead>
                     <tbody>
-                        {Array(300)
+                        {Array(100)
                             .fill(null)
                             .map((item, index) => (
                                 <tr key={index}>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
-                                    <td>10000000000</td>
+                                    <td>{index}</td>
+                                    <td>Michel</td>
+                                    <td>James</td>
+                                    <td>Jackson</td>
+                                    <td>1123&nbsp;1234</td>
+                                    <td>Big&nbsp;Hot&nbsp;Dog</td>
+                                    <td>Ap&nbsp;#190-9925&nbsp;n&nbsp;Road</td>
+                                    <td>USA,&nbsp;Florida</td>
                                 </tr>
                             ))}
                     </tbody>
