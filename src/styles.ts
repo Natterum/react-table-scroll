8a8399beb52b2749f6551ec5844e46dc28dbc9f6
Natterum <fast7creation@gmail.com>
2023-07-17 11:43:40 +0200

    Add styles file

---
+++ src/styles.ts
@@ -0,0 +1,25 @@
+import { CSSProperties } from 'react';
+
+export const rtsTableWrapper: CSSProperties = {
+    overflowX: 'hidden',
+    position: 'relative',
+    width: '100%'
+};
+
+export const rtsTableContainer: CSSProperties = {
+    minWidth: 'fit-content',
+    position: 'relative'
+};
+
+export const rtsTableScrollWrapper: CSSProperties = {
+    overflowX: 'scroll',
+    overflowY: 'hidden',
+    width: '100%',
+    position: 'sticky',
+    bottom: 0
+};
+
+export const rtsTableScrollContent: CSSProperties = {
+    height: '1px',
+    width: 0
+};
