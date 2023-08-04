a9d31c607196e8921843e55c7e38fdb61741db75
Natterum <fast7creation@gmail.com>
2023-08-04 12:21:00 +0200

    Update build config

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const nextConfig = {
+    output: 'export',
+    basePath: '/react-table-scroll',
+    images: {
+        unoptimized: true
+    }
+};
 
 module.exports = nextConfig;
